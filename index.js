9944ffa1b9d331725640cb3c238684c19b7147b3
Acer <codecinnpro@gmail.com>
2023-12-25 03:04:15 +0800

    6

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const swaggerUi = require("swagger-ui-express");
 const swaggerJSDoc = require('swagger-jsdoc');
-const loginRouter  = require('./routes/login');
 
 //must be on top, before all route
 app.use(express.json());
@@ -24,8 +23,6 @@ app.get('/', (req, res) => {
 app.get('/haha', (req, res) => {
     res.send('hahahahahahahahahahahaahahahaha')
   })
-
-app.use('/login', loginRouter);
 
 const options = {
     definition:{
@@ -65,8 +62,97 @@ const options = {
         ],
     },
     //all the route.js file store inside the route file 
-    apis:["./routes/*.js"],
+    apis:["./index.js"],
 };
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Visit:
+ *       type: object
+ *       required:
+ *         - purposeOfVisit
+ *         - phoneNumber
+ *       properties:
+ *         purposeOfVisit:
+ *           type: string
+ *           description: The purpose of the visit
+ *         phoneNumber:
+ *           type: number
+ *           description: The phone number of the visitor
+ *         visitTime:
+ *           type: string
+ *           format: date-time
+ *           description: The time of the visit
+ *       example:
+ *         purposeOfVisit: Meeting
+ *         phoneNumber: 1234567890
+ *
+ *     Visitor:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the visitor
+ *         visits:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Visit'
+ *       example:
+ *         name: John Doe
+ *         visits:
+ *           - purposeOfVisit: Meeting
+ *             phoneNumber: 1234567890
+ *             visitTime: '2023-01-01T12:00:00Z'
+ *
+ *     User:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *         - email
+ *         - phoneNumber
+ *         - category
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: The username of the user
+ *         password:
+ *           type: string
+ *           description: The password of the user
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: The email address of the user
+ *         phoneNumber:
+ *           type: number
+ *           description: The phone number of the user
+ *         category:
+ *           type: string
+ *           enum:
+ *             - host
+ *             - admin
+ *           description: The category of the user (host or admin)
+ *         visitors:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Visitor'
+ *       example:
+ *         username: user123
+ *         password: password123
+ *         email: user@exa\mple.com
+ *         phoneNumber: 1234567890
+ *         category: host
+ *         visitors:
+ *           - name: John Doe
+ *             visits:
+ *               - purposeOfVisit: Meeting
+ *                 phoneNumber: 1234567890
+ *                 visitTime: '2023-01-01T12:00:00Z'
+ */
 
 const spacs = swaggerJSDoc(options);
 app.use("/g6", swaggerUi.serve, swaggerUi.setup(spacs));
